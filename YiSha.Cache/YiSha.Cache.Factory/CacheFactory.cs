90306a0de1b498a248dc15460e2f044db62b0adc
chao liu <greatboylc@hotmail.com>
2021-01-25 12:13:08 +0800

    添加 YiShaAdmin.v2.2 代码 提交

---
+++ YiSha.Cache/YiSha.Cache.Factory/CacheFactory.cs
@@ -0,0 +1,13 @@
+﻿using YiSha.Cache.Interface;
+using YiSha.MemoryCache;
+
+namespace YiSha.Cache.Factory
+{
+    public class CacheFactory
+    {
+        public static ICache Cache()
+        {
+            return new MemoryCacheImp();
+        }
+    }
+}
